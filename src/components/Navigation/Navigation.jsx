8a8c1629161a1243b46b3ad61213110bfb8ac909
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-07-01 01:23:18 -0400

    more media query work

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,6 +5,39 @@ import { useLocation, Link } from 'react-router-dom';
 
 // import { ThemeContext } from '../../contexts/theme'
 
+// Nav hamburgerburger selections
+// const burger = document.querySelector("#burger-menu");
+// const ul = document.querySelector("nav ul");
+
+
+// // Scroll to top selection
+// const scrollUp = document.querySelector("#scroll-up");
+
+// // Select nav links
+// const navLink = document.querySelectorAll(".nav-link");
+
+// // Hamburger menu function
+// burger.addEventListener("click", () => {
+//   ul.classList.toggle("show");
+// });
+
+// // Close hamburger menu when a link is clicked
+// navLink.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     ul.classList.remove("show");
+//   })
+// );
+
+// // scroll to top functionality
+// scrollUp.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
+
+
 
 const Navigation = () => {
     // const [{ themeName, toggleTheme }] = useContext(ThemeContext)
@@ -26,14 +59,6 @@ return(
 <li> <Link className="nav-link" to="/contact">Contact</Link></li>
 <li><a href="https://drive.google.com/file/d/1vZQBc5jUAcxtYmAGdZeAWKi2nOuuUJ2f/view?usp=sharing" className="nav-link">Resume</a></li>
 </ul>
-{/* <button
-        type='button'
-        onClick={toggleTheme}
-        className='btn btn--icon nav__theme'
-        aria-label='toggle theme'
-      >
-        {themeName === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
-      </button> */}
 <button className="burger-menu" id="burger-menu">
 <ion-icon class="bars" name="menu-outline"></ion-icon>
 <ion-icon class="times" name="close-outline"></ion-icon>
--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,33 @@
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
+// // Nav hamburgerburger selections
+// const burger = document.querySelector("#burger-menu");
+// const ul = document.querySelector("nav ul");
 
 
-// Scroll to top selection
-const scrollUp = document.querySelector("#scroll-up");
+// // Scroll to top selection
+// const scrollUp = document.querySelector("#scroll-up");
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
+// // Select nav links
+// const navLink = document.querySelectorAll(".nav-link");
 
-// Hamburger menu function
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
+// // Hamburger menu function
+// burger.addEventListener("click", () => {
+//   ul.classList.toggle("show");
+// });
 
-// Close hamburger menu when a link is clicked
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
+// // Close hamburger menu when a link is clicked
+// navLink.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     ul.classList.remove("show");
+//   })
+// );
 
-// scroll to top functionality
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
+// // scroll to top functionality
+// scrollUp.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
 
 
